c51a55bb8e982d3952edf8adc443dd3356c7fd2e
ValentinaRippe <lauravalentina200102@gmail.com>
2024-02-27 22:18:03 -0500

    Added best practices

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const prouteProduct = require("./routes/product.route")
+const PORT = 4000
 //Setings
-app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || PORT);
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
 
 //routes
-app.use("/api/items", require("./routes/product.route"));
+app.use("/api/items", prouteProduct);
 
 module.exports = app;
